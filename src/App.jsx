e7a11ef41b7405b8bf45139165603412bd822558
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-10-24 23:19:46 +0600

    try

--- src/App.jsx
+++ src/App.jsx
@@ -24,5 +24,20 @@ function App() {
 
   return <RouterProvider router={router} />;
 }
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/registration" element={<Registration />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+}
 
 export default App;
