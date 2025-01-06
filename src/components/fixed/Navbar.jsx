376493d404e90c3219744d895118131a315e7089
tamimalhridoy <tamimalhridoy3@gmail.com>
2025-01-06 17:04:20 +0600

    start now

--- src/components/fixed/Navbar.jsx
+++ src/components/fixed/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div className="container"></div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
